e33c4a52697cac377f072e1da45c520ab79f80a6
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-05-30 23:48:23 -0300

    Add colors and shades

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DemoViewer {
     public static void main(String[] args) {
@@ -32,21 +34,18 @@ public class DemoViewer {
                         new Vertex(-100, 100, -100),
                         Color.WHITE
                 ));
-
                 tris.add(new Triangle(
                         new Vertex(100, 100, 100),
                         new Vertex(-100, -100, 100),
                         new Vertex(100, -100, -100),
                         Color.RED
                 ));
-
                 tris.add(new Triangle(
                         new Vertex(-100, 100, -100),
                         new Vertex(100, -100, -100),
                         new Vertex(100, 100, 100),
                         Color.GREEN
                 ));
-
                 tris.add(new Triangle(
                         new Vertex(-100, 100, -100),
                         new Vertex(100, -100, -100),
@@ -55,12 +54,6 @@ public class DemoViewer {
                 ));
 
                 double heading = Math.toRadians(headingSlider.getValue());
-
-//                Matrix3 transform = new Matrix3(new double[] {
-//                        Math.cos(heading), 0, -Math.sin(heading),
-//                        0, 1, 0,
-//                        Math.sin(heading), 0, Math.cos(heading)
-//                });
 
                 Matrix3 headingTransform = new Matrix3(new double[] {
                         Math.cos(heading), 0, Math.sin(heading),
@@ -78,34 +71,80 @@ public class DemoViewer {
 
                 Matrix3 transform = headingTransform.multiply(pitchTransform);
 
-                graph2.translate(getWidth() / 2, getHeight() / 2);
-                graph2.setColor(Color.WHITE);
+//                graph2.translate(getWidth() / 2, getHeight() / 2);
+//                graph2.setColor(Color.WHITE);
 
-//                for (Triangle triangle: tris) {
-//                    Path2D path = new Path2D.Double();
-//                    path.moveTo(triangle.v1.x, triangle.v1.y);
-//                    path.lineTo(triangle.v2.x, triangle.v2.y);
-//                    path.lineTo(triangle.v3.x, triangle.v3.y);
-//
-//                    path.closePath();
-//                    graph2.draw(path);
-//                }
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+
+                Arrays.fill(zBuffer, Double.NEGATIVE_INFINITY);
 
                 for (Triangle triangle: tris) {
                     Vertex v1 = transform.transform(triangle.v1);
+                    v1.x += getWidth() / 2.0;
+                    v1.y += getHeight() / 2.0;
                     Vertex v2 = transform.transform(triangle.v2);
+                    v2.x += getWidth() / 2.0;
+                    v2.y += getHeight() / 2.0;
                     Vertex v3 = transform.transform(triangle.v3);
+                    v3.x += getWidth() / 2.0;
+                    v3.y += getHeight() / 2.0;
 
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
+                    Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                    Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
 
-                    path.closePath();
-                    graph2.draw(path);
+                    Vertex norm = new Vertex(
+                            ab.y * ac.z - ab.z * ac.y,
+                            ab.z * ac.x - ab.x * ac.z,
+                            ab.x * ac.y - ab.y * ac.x
+                    );
+
+                    double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                    norm.x /= normalLength;
+                    norm.y /= normalLength;
+                    norm.z /= normalLength;
+
+                    double angleCos = Math.abs(norm.z);
+
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <= maxX; x++) {
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+
+                                if (zBuffer[zIndex] < depth) {
+//                                    img.setRGB(x, y, triangle.color.getRGB());
+                                    img.setRGB(x, y, getShade(triangle.color, angleCos).getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
+                            }
+                        }
+                    }
+
+//                    Path2D path = new Path2D.Double();
+//                    path.moveTo(v1.x, v1.y);
+//                    path.lineTo(v2.x, v2.y);
+//                    path.lineTo(v3.x, v3.y);
+//
+//                    path.closePath();
+//                    graph2.draw(path);
                 }
 
+                graph2.drawImage(img, 0, 0, null);
             }
+
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
@@ -115,5 +154,17 @@ public class DemoViewer {
         frame.setSize(400, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1/2.4);
+        int green = (int) Math.pow(greenLinear, 1/2.4);
+        int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+        return new Color(red, green, blue);
     }
 }
